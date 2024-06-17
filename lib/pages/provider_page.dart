ec84a5d1cfb593f6d9b1e3f1640b0dbca18919a5
sriharshan136 <sriharshan136@gmail.com>
2024-06-17 11:07:34 -0700

    providers page updated

--- lib/pages/provider_page.dart
+++ lib/pages/provider_page.dart
@@ -13,7 +13,9 @@ class ProviderPage extends StatefulWidget {
 class _ProviderPageState extends State<ProviderPage> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     String? name;
+    String? imgPath;
 
     // Extract details from the list
     for (var detail in widget.details) {
@@ -21,21 +23,60 @@ class _ProviderPageState extends State<ProviderPage> {
         case 'name':
           name = detail.value;
           break;
+        case 'picture':
+          imgPath = detail.value;
+          break;
       }
     }
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: bgColor,
-        elevation: 0,
-        leading: const BackButton(
-          color: Colors.black,
-        ),
-        title: Text(
-          name!,
-          style: const TextStyle(fontWeight: FontWeight.bold),
-        ),
-      ),
       backgroundColor: bgColor,
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Hero(
+                        tag: name!,
+                        child: Image.asset(
+                          imgPath!,
+                          fit: BoxFit.fitWidth,
+                          alignment: Alignment.topCenter,
+                          height: size.height * 0.6,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            top: 40.0, // Adjusted for better visibility
+            left: 16.0,
+            child: InkWell(
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+              child: Container(
+                padding: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.7),
+                  shape: BoxShape.circle,
+                ),
+                child: const Icon(
+                  Icons.arrow_back,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
+
