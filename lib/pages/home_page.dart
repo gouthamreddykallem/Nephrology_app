89883387adbebe126e7513e3a3ed8a1491813241
sriharshan136 <sriharshan136@gmail.com>
2024-06-18 23:35:05 -0700

    Home page updated v2

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,48 +4,71 @@ import 'package:nephrology_app/shared/color.dart';
 import 'package:nephrology_app/shared/data.dart';
 import 'package:nephrology_app/utilities/utils.dart';
 
-class HomeBody extends StatelessWidget {
+class HomeBody extends StatefulWidget {
   const HomeBody({super.key});
 
   @override
+  _HomeBodyState createState() => _HomeBodyState();
+}
+
+class _HomeBodyState extends State<HomeBody> {
+  @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: ListView(
-      children: [
-        searchBar(context),
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              buildButton(context, 'PATIENT PORTAL'),
-              buildButton(context, 'REFER A PATIENT'),
-            ],
+      child: ListView(
+        children: [
+          searchBar(context),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Expanded(
+                      child: Category(
+                        title: "Services",
+                        categories: kidneyServices,
+                        iconPath: "assets/kidneyIcon.svg",
+                      ),
+                    ),
+                    const SizedBox(width: 16.0),
+                    Expanded(
+                      child: Category(
+                        title: "About Us",
+                        categories: aboutUs,
+                        iconPath: "assets/aboutus.svg",
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
-        ),
-        Category(title: "Kidney Services", categories: kidneyServices),
-        Category(title: "Vascular Access", categories: vascularAccess),
-        Category(title: "About Us", categories: aboutUs),
-      ],
-    ));
+        ],
+      ),
+    );
   }
 }
 
 Widget searchBar(BuildContext context) {
   return Container(
     decoration: const BoxDecoration(
-        gradient: LinearGradient(
-            colors: [primaryColorLight, primaryColor],
-            begin: FractionalOffset(0.0, 0.0),
-            end: FractionalOffset(1.0, 0.0),
-            stops: [0.0, 1.0],
-            tileMode: TileMode.clamp),
-        borderRadius: BorderRadius.only(
-            bottomRight: Radius.circular(26), bottomLeft: Radius.circular(26))),
+      gradient: LinearGradient(
+        colors: [primaryColorLight, primaryColor],
+        begin: FractionalOffset(0.0, 0.0),
+        end: FractionalOffset(1.0, 0.0),
+        stops: [0.0, 1.0],
+        tileMode: TileMode.clamp,
+      ),
+      borderRadius: BorderRadius.only(
+        bottomRight: Radius.circular(26),
+        bottomLeft: Radius.circular(26),
+      ),
+    ),
     child: Padding(
       padding: const EdgeInsets.only(top: 8, left: 8, right: 8, bottom: 10),
       child: Container(
-        // height: 50,
         decoration: BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.circular(26),
@@ -73,27 +96,23 @@ Widget buildButton(BuildContext context, String text) {
   return ElevatedButton(
     style: ElevatedButton.styleFrom(
       foregroundColor: Colors.white,
-      backgroundColor: primaryColor, // Text color
-      shadowColor: Colors.blueAccent, // Shadow color
-      elevation: 8, // Elevation for 3D effect
+      backgroundColor: primaryColor,
+      shadowColor: Colors.blueAccent,
+      elevation: 8,
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8), // Rounded corners
+        borderRadius: BorderRadius.circular(8),
       ),
-      padding:
-          const EdgeInsets.symmetric(horizontal: 24, vertical: 16), // Padding
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
     ),
     onPressed: () {
-      if(text=="PATIENT PORTAL"){
-        Utilities.urlLauncher(Uri.parse("https://www.myhealthrecord.com/Portal/SSO"));
+      if (text == "PATIENT PORTAL") {
+        Utilities.urlLauncher(
+            Uri.parse("https://www.myhealthrecord.com/Portal/SSO"));
+      } else {
+        Utilities.urlLauncher(Uri.parse(
+            "https://www.thenephrologygroupinc.com/Portals/0/Online%20Forms/Forms%202-12-2018/NewPatientRF.pdf?ver=2018-02-16-140849-517"));
       }
-      else{
-        Utilities.urlLauncher(Uri.parse("https://www.thenephrologygroupinc.com/Portals/0/Online%20Forms/Forms%202-12-2018/NewPatientRF.pdf?ver=2018-02-16-140849-517"));
-      }
-      // Button pressed action
-      // ScaffoldMessenger.of(context).showSnackBar(
-      //   SnackBar(content: Text('$text pressed')),
-      // );
     },
-    child: Text(text, style: const TextStyle(fontWeight: FontWeight.bold))
+    child: Text(text, style: const TextStyle(fontWeight: FontWeight.bold)),
   );
 }
--- lib/pages/layout.dart
+++ lib/pages/layout.dart
@@ -3,10 +3,7 @@ import 'package:nephrology_app/components/floating_buttons.dart';
 import 'package:nephrology_app/components/side_drawer.dart';
 import 'package:nephrology_app/shared/color.dart';
 
-import 'ai_chat_page.dart';
-import 'education_page.dart';
 import 'home_page.dart';
-import 'login_page.dart';
 
 class LayOut extends StatefulWidget {
   const LayOut({super.key});
@@ -16,34 +13,32 @@ class LayOut extends StatefulWidget {
 }
 
 class _LayOutState extends State<LayOut> {
-  int _selectedIndex = 0;
-
-  static const List<Widget> _widgetOptions = <Widget>[
-    HomeBody(),
-    EducationBody(),
-    LoginBody(),
-    AiChatBody(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white,
+        backgroundColor: primaryColor,
         iconTheme: const IconThemeData(color: Colors.white),
-        elevation: 0.0,
-        title: Flexible(
-          child: Image.asset('assets/logo.jpg',
-              fit: BoxFit.cover, alignment: Alignment.center),
+        elevation: 0.5,
+        title: const Center(
+          child: Text(
+            "THE NEPHROLOGY GROUP, INC",
+            style: TextStyle(fontSize: 20.0, color: Colors.white),
+          ),
         ),
-        // const Text("The Nephrology Group, Inc",
-        //     style: TextStyle(color: Colors.white), textDirection: TextDirection.ltr),
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: [
+                  primaryColor,
+                  primaryColorLight,
+                ],
+                begin: FractionalOffset(0.0, 0.0),
+                end: FractionalOffset(1.0, 0.0),
+                stops: [0.0, 1.0],
+                tileMode: TileMode.clamp),
+          ),
+        ),
         leading: Builder(
           builder: (context) {
             return IconButton(
@@ -73,40 +68,8 @@ class _LayOutState extends State<LayOut> {
       ),
       drawer: const SideDrawer(),
       floatingActionButton: const FloatingButtons(),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedFontSize: 20,
-        selectedIconTheme:
-            const IconThemeData(color: primaryColorLight, size: 40),
-        selectedItemColor: Colors.blue,
-        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
-        unselectedFontSize: 20,
-        unselectedIconTheme:
-            const IconThemeData(color: secondaryColor, size: 40),
-        unselectedItemColor: Colors.blue,
-        unselectedLabelStyle: const TextStyle(fontWeight: FontWeight.normal),
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            label: 'Home',
-            icon: Icon(Icons.home),
-          ),
-          BottomNavigationBarItem(
-            label: 'Education',
-            icon: Icon(Icons.cast_for_education),
-          ),
-          BottomNavigationBarItem(
-            label: 'Account',
-            icon: Icon(Icons.account_circle_outlined),
-          ),
-          BottomNavigationBarItem(
-            label: 'AI chat',
-            icon: Icon(Icons.cloud_circle_rounded),
-          )
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
+      body: const Center(
+        child: HomeBody(),
       ),
     );
   }
