7f06aefc4c5666176a9c2dc8c3a50033d1f9643f
sriharshan136 <sriharshan136@gmail.com>
2024-06-20 22:40:36 -0700

    Home page updated v3

--- lib/components/floating_buttons.dart
+++ lib/components/floating_buttons.dart
@@ -1,50 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:nephrology_app/pages/ai_chat_page.dart';
 import 'package:nephrology_app/utilities/utils.dart';
-
-
 
 class FloatingButtons extends StatelessWidget {
   const FloatingButtons({super.key});
-//  static urlLauncher(Uri url) async {
-//     // const url = ;   
-//     if (await canLaunchUrl(url)) {
-//        await launchUrl(url);
-//     } else {
-//       throw 'Could not launch $url';
-//     }   
-// }
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-    // mainAxisAlignment: MainAxisAlignment.end,
-    padding: const EdgeInsets.only(left: 30, bottom: 50),
-    child : Row(
-    children: [
-      FloatingActionButton(
-        onPressed: () {
-          Utilities.urlLauncher( Uri(scheme: 'tel', path: '+1-559-228-6600'));
-          //...
-        },
-        tooltip: 'Contact Us',
-        heroTag: null,
-        child: const Icon(
-          Icons.call
-        ),
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              Utilities.urlLauncher(
+                  Uri(scheme: 'tel', path: '+1-559-228-6600'));
+            },
+            tooltip: 'Contact Us',
+            heroTag: 'contactUs',
+            child: const Icon(Icons.call),
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Dialog(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: const ChatDialog(),
+                    ),
+                  );
+                },
+              );
+            },
+            tooltip: 'Chat Bot',
+            heroTag: 'chatBot',
+            child: const Icon(Icons.chat_rounded),
+          ),
+        ],
       ),
-      const SizedBox(
-        width: 300,
-      ),
-      // FloatingActionButton(           
-      //   onPressed: () {
-
-      //   },
-      //   heroTag: null,           
-      //   child: const Icon(
-      //     Icons.chat
-      //   ),
-      // )
-    ]
-    )
-  );
+    );
   }
 }
+
+class ChatDialog extends StatelessWidget {
+  const ChatDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.8,
+      width: MediaQuery.of(context).size.width * 0.8,
+      padding: const EdgeInsets.all(0.0),
+      child: const AiChatBody(),
+    );
+  }
+}
+
