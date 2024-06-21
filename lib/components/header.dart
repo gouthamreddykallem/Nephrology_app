4b6d717cfaf28a6fc6d137ff3f88d52e8bc80fcd
sriharshan136 <sriharshan136@gmail.com>
2024-06-20 21:57:03 -0700

    Home page updated v3

--- lib/components/header.dart
+++ lib/components/header.dart
@@ -19,8 +19,7 @@ class _HeaderState extends State<Header> with SingleTickerProviderStateMixin {
       child: Column(
         children: [
           Container(
-            padding: const EdgeInsets.only(
-                left: 16.0, right: 16.0, top: 88.0, bottom: 4.0),
+            padding: const EdgeInsets.only(top: 88.0, bottom: 4.0),
             width: double.infinity,
             decoration: BoxDecoration(
               gradient: const LinearGradient(
@@ -43,59 +42,123 @@ class _HeaderState extends State<Header> with SingleTickerProviderStateMixin {
                 ),
               ],
             ),
-            child: AnimatedSize(
-              duration: const Duration(milliseconds: 300),
-              curve: Curves.easeInOut,
-              child: Column(
-                children: [
-                  if (isExpanded) ...[
-                    const Text(
-                      "WELCOME\nTO TNG",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 40.0,
-                        fontFamily: 'Poppins',
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: CircleAvatar(
-                        backgroundColor: Colors.white,
-                        radius: MediaQuery.of(context).size.height * .12,
-                        child: Image.asset(
-                          "assets/logo_2.jpg",
-                          fit: BoxFit.fill,
+            child: Stack(
+              children: [
+                // Positioned arcs
+                Positioned(
+                  top: -50,
+                  left: -80,
+                  child: CircleAvatar(
+                    radius: 100,
+                    backgroundColor: Colors.white.withOpacity(0.1),
+                  ),
+                ),
+                Positioned(
+                  top: 50,
+                  right: 40,
+                  child: CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Colors.white.withOpacity(0.1),
+                  ),
+                ),
+                Positioned(
+                  top: 400,
+                  left: -80,
+                  child: CircleAvatar(
+                    radius: 150,
+                    backgroundColor: Colors.white.withOpacity(0.1),
+                  ),
+                ),
+                Positioned(
+                  top: 300,
+                  right: -80,
+                  child: CircleAvatar(
+                    radius: 100,
+                    backgroundColor: Colors.white.withOpacity(0.1),
+                  ),
+                ),
+                AnimatedSize(
+                  duration: const Duration(milliseconds: 300),
+                  curve: Curves.easeInOut,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+                    child: Column(
+                      children: [
+                        if (isExpanded) ...[
+                          const Text(
+                            "WELCOME\nTO TNG",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 40.0,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ],
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              left: 8.0, right: 8.0, bottom: 8.0, top: 16.0),
+                          child: Center(
+                            child: AnimatedContainer(
+                              duration: const Duration(milliseconds: 300),
+                              curve: Curves.easeInOut,
+                              width: isExpanded
+                                  ? MediaQuery.of(context).size.height * .24
+                                  : MediaQuery.of(context).size.height * .12,
+                              height: isExpanded
+                                  ? MediaQuery.of(context).size.height * .24
+                                  : MediaQuery.of(context).size.height * .12,
+                              child: CircleAvatar(
+                                backgroundColor: Colors.white,
+                                radius: isExpanded
+                                    ? MediaQuery.of(context).size.height * .12
+                                    : MediaQuery.of(context).size.height * .06,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(4.0),
+                                  child: ClipOval(
+                                    child: Image.asset(
+                                      "assets/logo_2.jpg",
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                    const Text(
-                      "The Nephrology Group, Inc. (TNG) is Central California’s largest Nephrology Practice. Since founded in 1975, our physicians are committed to providing quality care to patients with acute kidney",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.normal,
-                      ),
-                    ),
-                    const SizedBox(height: 8.0),
-                  ],
-                  InkWell(
-                    onTap: () {
-                      setState(() {
-                        isExpanded = !isExpanded;
-                      });
-                    },
-                    child: Icon(
-                      size: 40.0,
-                      //isExpanded ? Icons.expand_less : Icons.expand_more,
-                      isExpanded ? Icons.arrow_drop_up_sharp : Icons.arrow_drop_down_sharp,
-                      color: Colors.white,
+                        if (isExpanded) ...[
+                          const SizedBox(height: 8.0),
+                          const Text(
+                            "The Nephrology Group, Inc. (TNG) is Central California’s largest Nephrology Practice. Since founded in 1975, our physicians are committed to providing quality care to patients with acute kidney",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.normal,
+                            ),
+                          ),
+                          const SizedBox(height: 8.0),
+                        ],
+                        InkWell(
+                          onTap: () {
+                            setState(() {
+                              isExpanded = !isExpanded;
+                            });
+                          },
+                          child: Icon(
+                            size: 40.0,
+                            isExpanded
+                                ? Icons.arrow_drop_up_sharp
+                                : Icons.arrow_drop_down_sharp,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ],
@@ -103,4 +166,3 @@ class _HeaderState extends State<Header> with SingleTickerProviderStateMixin {
     );
   }
 }
-
